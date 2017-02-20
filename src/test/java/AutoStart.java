3d305a9024a52c3bb1735ec5285160da6e0c1437
akorshun92 <akorshun92@gmail.com>
2017-02-20 15:45:12 +0200

    test

--- src/test/java/AutoStart.java
+++ src/test/java/AutoStart.java
@@ -1,5 +1,11 @@
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.json.JSONException;
 import org.json.JSONObject;
-import org.testng.annotations.Test;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -7,11 +13,45 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
+import static com.jayway.restassured.RestAssured.given;
+import static junit.framework.TestCase.assertTrue;
+
 /**
  * Created by korshun on 1/5/2017.
  */
 
-public class AutoStart{
+public class AutoStart {
+
+    public static final String ACCESS_KEY = "";
+    //  public static final String BASE_URL = "https://api.auto.ru/rest/?sid=&client_tz=180&method=api.service.getUuid&client_version=3.10.1&key=1d2b14555a83699f57fd77d17aa2d5ce9431cd7d9f3edea14186b044e76b606a&client_os=6.0.1&version=2.2.2&device_name=motorola%20XT1562&client_platform=android&format=json";
+    public static JSONObject json;
+    public static String sid;
+    public static String uuid_header;
+    public static String uuid;
+    public static String autoruuid;
+    public static String x_auth;
+    public static String auth_sid;
+    public static String auth_sid_key;
+    public static String auth_autoruuid;
+    public static long millis = System.currentTimeMillis();
+    public static String url_api2 = "https://api2.auto.ru/1.1/search?category_id=15&page_num=1&page_size=50&creation_date_to=" + millis;
+    String username = "yuioru@yandex.ru";
+    String password = "111111";
+
+    static CloseableHttpClient client = HttpClients.createDefault();
+
+
+    @BeforeClass
+    public static void beforeClass() throws IOException, JSONException {
+        json = FirstConnectJson.json();
+        sid = String.valueOf(json.get("sid"));
+        uuid_header = "OAuth" + " " + String.valueOf(json.getJSONObject("result").get("uuid"));
+        uuid = String.valueOf(json.getJSONObject("result").get("uuid"));
+        x_auth = "Vertis" + " " + String.valueOf(json.getJSONObject("result").get("uuid")) + " " + "5c27f9e8-2b90-433e-a0bf-b5222bbd97d0";
+        autoruuid = String.valueOf(json.get("autoruuid"));
+    }
+
+
     @Test
     public void aptTesting() throws Exception {
         try {
@@ -30,15 +70,15 @@ public class AutoStart{
             while (scan.hasNext())
                 entireResponse += scan.nextLine();
 
-            System.out.println("Response : "+entireResponse);
+            System.out.println("Response : " + entireResponse);
 
             scan.close();
 
-            JSONObject obj = new JSONObject(entireResponse );
+            JSONObject obj = new JSONObject(entireResponse);
             /*String responseCode = obj.getString("status");
             System.out.println("status : " + responseCode);*/
 
-            JSONObject full = new JSONObject(entireResponse );
+            JSONObject full = new JSONObject(entireResponse);
             JSONObject obj1 = full.getJSONObject("result");
             String totalRes = obj1.getString("uuid");
             //System.out.println("uuid : " + totalRes);
@@ -51,8 +91,6 @@ public class AutoStart{
             System.out.println(secauth);
 
 
-
-
             conn.disconnect();
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -63,5 +101,39 @@ public class AutoStart{
 
         }
 
+    }
+
+    @Test //Авторизация пользователя
+    public void autorize() {
+        RestAssured.baseURI = "https://api.auto.ru";
+        Response r = given().headers("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8").
+                body("login=" + username + "&pass=" + password + "&sid=" + sid + "&method=users.auth.login&key=1d2b14555a83699f57fd77d17aa2d5ce9431cd7d9f3edea14186b044e76b606a" + "&version=2.2.2&uuid=" + uuid + "&format=json").
+                when().post("/rest/");
+        assertTrue(r.statusCode() == 200);
+        // System.out.println(r.body().asString());
+        auth_sid = r.body().jsonPath().get("sid");
+        auth_sid_key = r.body().jsonPath().get("sid_key");
+        auth_autoruuid = r.body().jsonPath().get("autoruuid");
+    }
+
+    @Test //Ошибка логина
+    public void badautorize() {
+        String username = "yuioru123@yandex.ru";
+
+        RestAssured.baseURI = "https://api.auto.ru";
+        Response r = given().headers("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8").
+                body("login=" + username + "&pass=" + password + "&sid=" + sid + "&method=users.auth.login&key=1d2b14555a83699f57fd77d17aa2d5ce9431cd7d9f3edea14186b044e76b606a" + "&version=2.2.2&uuid=" + uuid + "&format=json").
+                when().post("/rest/");
+        Object message = r.body().jsonPath().get("error.message");
+        //String error = r.body("error", ).jsonPath().get("error");
+        //Response message = r.body(error).jsonPath("message");
+        System.out.print(message);
+        assertTrue(r.statusCode() == 200);
+        assertTrue(message.equals("Неверный логин или пароль."));
+        // System.out.println(r.body().asString());
+        auth_sid = r.body().jsonPath().get("sid");
+        auth_sid_key = r.body().jsonPath().get("sid_key");
+        auth_autoruuid = r.body().jsonPath().get("autoruuid");
+        System.out.println(r.body().asString());
     }
 }
